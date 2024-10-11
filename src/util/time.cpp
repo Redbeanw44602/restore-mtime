abc0e10d61aa5b3ab3b0d41dccee4ecdfe30c17d
Redbeanw44602 <redbeana44945@gmail.com>
2024-10-11 23:15:27 +0800

    fix: correct utc timestamp.

--- src/util/time.cpp
+++ src/util/time.cpp
@@ -2,16 +2,19 @@
 
 namespace util::time {
 
+constexpr time_t std_time_point_to_timestamp_second_offset = 6437635199;
+
 std::filesystem::file_time_type to_filetime(time_t timestamp) {
-    auto time_point = std::chrono::system_clock::from_time_t(timestamp);
-    auto duration   = std::chrono::time_point_cast<std::filesystem::file_time_type::duration>(time_point);
+    timestamp       -= std_time_point_to_timestamp_second_offset;
+    auto time_point  = std::chrono::system_clock::from_time_t(timestamp);
+    auto duration    = std::chrono::time_point_cast<std::filesystem::file_time_type::duration>(time_point);
     return std::filesystem::file_time_type(duration.time_since_epoch());
 }
 
 time_t from_filetime(const std::filesystem::file_time_type& filetime) {
     auto time_point = filetime.time_since_epoch();
     auto duration   = std::chrono::duration_cast<std::chrono::seconds>(time_point);
-    return duration.count();
+    return duration.count() + std_time_point_to_timestamp_second_offset;
 }
 
 } // namespace util::time
